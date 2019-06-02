02721609da9585779b12331e536d4ea476d05ad0
aborigen <egoraborin@gmail.com>
2019-06-03 00:57:49 +0300

    Bugs fixed

---
+++ lib/screens/items.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyItems extends StatefulWidget {
+  @override
+  _MyItemsState createState() => _MyItemsState();
+}
+
+class _MyItemsState extends State<MyItems> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+        actions: <Widget>[
+          Icon(Icons.help)
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('item'),
+          ),
+          Dismissible(key: Key('a'),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Icon(Icons.swap_horiz),
+                  Icon(Icons.swap_horiz),
+                  Icon(Icons.swap_horiz),
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
